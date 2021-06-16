3409c0f750e74d56489d26ad45cabb7536e6b4c6
MdKaioumislam5 <afrankaiyum34@gmail.com>
2021-06-16 18:13:28 +0600

    root api create

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
+app.get('/', (req, res) =>{
+  res.send("hello from db it's working working")
+})
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -42,18 +45,19 @@ client.connect(err => {
       })
   })
 
-  app.delete('delete/:id', (req, res) => {
-    // const id = ObjectID(req.params.id);
-    // console.log('delete this', id);
-    productCollection.deleteOne({ _id: ObjectID(req.params.id)})
-      .then( ( result) => {
-        console.log(result);
-      })
-  })
+  // app.delete('delete/:id', (req, res) => {
+  //   // const id = ObjectID(req.params.id);
+  //   // console.log('delete this', id);
+  //   productCollection.deleteOne({ _id: ObjectID(req.params.id)})
+  //     .then( ( result) => {
+  //       console.log(result);
+  // //     })
+  // })
 
 });
 
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT || port)
+  // , () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
